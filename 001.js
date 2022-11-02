f09fd50fe245ea8db6622595473c3152fea5117c
vicenta <vicenta@gmail.com>
2022-11-02 10:31:10 +0100

    Aprendiendo javascript

---
+++ 001.js
@@ -0,0 +1,7 @@
+console.log("hola desde javascipt");
+console.log("hola");
+var caja=0;
+caja=1;
+console.log(caja);
+caja=caja+1;
+console.log(caja);
---
+++ 002.js
@@ -0,0 +1,6 @@
+var numero1=0;
+var numero2=7;
+var suma=numero1+numero2;
+console.log(suma);
+var resta=numero1-numero2;
+console.log(resta);
